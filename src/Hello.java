c42d6a54ee7ec1482d04ba4616b1165256cfe83b
Dhananjay <dhananjaykulkarni2499@gmail.com>
2023-05-28 20:21:05 +0530

    28-05-2023 1st Java project.

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main( String[] arg){
+        Hello obj =new Hello();
+        obj.call();
+    }
+    public void call(){
+        System.out.print("Hello Niggas!");
+    }
+}
