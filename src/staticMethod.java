41d10c8d3c67a5fc2be53881aedaeecee5432ae5
Dhananjay <dhananjaykulkarni2499@gmail.com>
2023-05-29 10:38:45 +0530

    28-05-2023 Java project.

---
+++ src/staticMethod.java
@@ -0,0 +1,8 @@
+public class staticMethod {
+    public static void main(String[] a){
+        st_meth();
+    }
+    public static void st_meth(){
+        System.out.print("Static call!");
+    }
+}
