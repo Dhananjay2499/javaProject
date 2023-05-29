53ac25999809d9d3f56f4841e2e7487e9fc3dd5b
Dhananjay <dhananjaykulkarni2499@gmail.com>
2023-05-29 08:31:12 +0530

    28-05-2023 Java project.

---
+++ src/countNumber.java
@@ -0,0 +1,11 @@
+public class countNumber {
+    public static void main( String[] agr ){
+        countNumber obj = new countNumber();
+        obj.counter();
+    }
+    public void counter(){
+        for(int i =0; i <= 10 ; i++){
+            System.out.print(i);
+        }
+    }
+}
