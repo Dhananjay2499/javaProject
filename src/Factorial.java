624393b92222296517363c640321e8d2e92fe431
Dhananjay <dhananjaykulkarni2499@gmail.com>
2023-05-29 20:42:29 +0530

    28-05-2023 Java project.

--- src/Factorial.java
+++ src/Factorial.java
@@ -3,14 +3,19 @@ import java.util.Scanner;
 public class Factorial {
     public static void main(String[] arg) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Give a number ");
-        int input = sc.nextInt();
-        System.out.print("Factorial of "+input+" is " + fib(input));
+        System.out.print("Give a number to calculate factorial: ");
+        long input = sc.nextInt();
+
+        if (input < 0) {
+            System.out.print("Invalid number for a factorial");
+        } else {
+            System.out.print("Factorial of " + input + " is " + fib(input));
+        }
     }
 
-    public static int fib( int num) {
-        int store = 1;
-        for(int i = 1; i <= num; i++){
+    public static long fib(long num) {
+        long store = 1;
+        for (int i = 1; i <= num; i++) {
             store = store * i;
         }
         return store;
