5a1af13e492c084ba8be52cb1b0a00edfb2f3b92
Dhananjay <dhananjaykulkarni2499@gmail.com>
2023-05-30 20:49:43 +0530

    28-05-2023 Java project.

---
+++ src/Palindrome.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] a) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number greater than 9 = ");
+        int input = sc.nextInt();
+
+        if (input >= 9) {
+            int output = pal(input);
+            if (output==input) {
+                System.out.print(input + " number is a Palindrome!\n" + input + " | " + input);
+            } else {
+                System.out.print(input + " number is not Palindrome!");
+            }
+        } else {
+            System.out.print("Number is not greater than 9");
+        }
+    }
+
+    public static int pal(int get) {
+        int st = get;
+        int output = 0;
+        while (st > 0) {
+            int temp = st % 10;
+            output = (output * 10) + temp;
+            st = st / 10;
+        }
+        return output;
+    }
+}
