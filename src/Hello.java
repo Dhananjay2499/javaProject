49c695514c28092243836c07f15324f5ab983f9e
Dhananjay <dhananjaykulkarni2499@gmail.com>
2023-06-02 20:30:46 +0530

    28-05-2023 Java project.

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main( String[] arg){
         Hello obj = new Hello();
         obj.call();
+
     }
    public void call(){
         System.out.print( "Hello!" );
