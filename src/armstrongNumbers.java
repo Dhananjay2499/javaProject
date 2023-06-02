8c825fd6456a4dd4632d39f40d89ec17e6192693
Dhananjay <dhananjaykulkarni2499@gmail.com>
2023-06-02 10:16:00 +0530

    28-05-2023 Java project.

--- src/armstrongNumbers.java
+++ src/armstrongNumbers.java
@@ -5,7 +5,6 @@ public class armstrongNumbers {
         Scanner sc = new Scanner(System.in);
         System.out.print("Give a max range to check an Armstrong Number ");
         int input = sc.nextInt();
-        long start = System.currentTimeMillis();
         System.out.print("Armstrong Numbers up to given number-> ");
         for (int i = 1; i <= input; i++) {
             String count = String.valueOf(i);
@@ -14,10 +13,6 @@ public class armstrongNumbers {
                 System.out.print(i + " ");
             }
         }
-        long end = System.currentTimeMillis();
-        double execution = end - start;
-        System.out.println();
-        System.out.println("Execution time: " + execution / 1000 + " seconds");
     }
     public static boolean armSt(int input, int power) {
         int output = 0;
@@ -25,7 +20,7 @@ public class armstrongNumbers {
         while (input > 0) {
             int temp = (int) Math.pow(input % 10, power);
             output = output + temp;
-            input = input / 10;
+            input /= 10;
         }
         return (compare==output);
     }
