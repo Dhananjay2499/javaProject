6504272ebcaff7ce986e6c82a61e6372f9e702bf
Dhananjay <dhananjaykulkarni2499@gmail.com>
2023-06-05 19:25:22 +0530

    05-06-2023

--- src/strVarVsObj.java
+++ src/strVarVsObj.java
@@ -3,7 +3,7 @@ public class strVarVsObj {
         String string1 = "JAVA";
         String string2 = "JAVA";
         String string3 = string1;
-        /* All strings (created as a variable) will be pointing towards the same memory address */
+/*All strings (created as a variable) will be pointing towards the same memory address */
         System.out.println("-----------------------------------------------");
         System.out.println("\tstring1->" +"\"" +string1 + "\"->address : "+System.identityHashCode(string1));
         System.out.println("\tstring2->" +"\"" +string2 + "\"->address : "+System.identityHashCode(string2));
@@ -13,7 +13,7 @@ public class strVarVsObj {
         String string4 = new String("JAVA");
         String string5 = new String("JAVA");
         String string6 = new String(string1);
-        /* All strings (created as an object) will be pointing towards the different memory address */
+/*All strings (created as an object) will be pointing towards the different memory address */
         System.out.println("\tstring4->" +"\"" +string4 + "\"->address : "+System.identityHashCode(string4));
         System.out.println("\tstring5->" +"\"" +string5 + "\"->address : "+System.identityHashCode(string5));
         System.out.println("\tstring6->" +"string1" + "->address : "+System.identityHashCode(string6));
